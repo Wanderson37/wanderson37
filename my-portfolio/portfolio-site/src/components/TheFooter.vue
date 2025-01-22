c61b485566dbd6f352ecd790f20324e7777cff9a
Wanderson37 <wanderson_c.o@hotmail.com>
2025-01-22 10:47:32 -0400

    responsividade thefooter

--- my-portfolio/portfolio-site/src/components/TheFooter.vue
+++ my-portfolio/portfolio-site/src/components/TheFooter.vue
@@ -1,25 +1,29 @@
 <template>
-    <footer>
-      <p>&copy; 2024 My Portfolio. All Rights Reserved.</p>
-      <div>
-        <a href="https://github.com/Wanderson37" target="_blank">GitHub</a> | 
-        <a href="https://www.linkedin.com/in/wanderson-oliveira-06a1b1196/" target="_blank">LinkedIn</a>
-      </div>
-    </footer>
-  </template>
-  
-  <style>
-  footer {
-    text-align: center;
-    padding: 20px;
-    background-color: #282c34;
-    color: white;
-  }
-  footer a {
-    color: #42b983;
-    text-decoration: none;
-  }
-  footer a:hover {
-    text-decoration: underline;
-  }
-  </style>
+  <footer class="bg-primary shadown-17">
+    <p>&copy; 2024 My Portfolio. All Rights Reserved.</p>
+    <div>
+      <a href="https://github.com/Wanderson37" target="_blank">GitHub</a> |
+      <a
+        href="https://www.linkedin.com/in/wanderson-oliveira-06a1b1196/"
+        target="_blank"
+        >LinkedIn</a
+      >
+    </div>
+  </footer>
+</template>
+
+<style>
+footer {
+  text-align: center;
+  padding: 20px;
+  color: white;
+  border-radius: 1rem;
+}
+footer a {
+  color: #42b983;
+  text-decoration: none;
+}
+footer a:hover {
+  text-decoration: underline;
+}
+</style>
