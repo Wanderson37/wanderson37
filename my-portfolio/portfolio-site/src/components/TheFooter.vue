cb502a4fbf2d99ccff3445e0d7727a3ef7145690
Wanderson37 <wanderson_c.o@hotmail.com>
2025-01-23 14:25:04 -0400

    att logo thefooter

--- my-portfolio/portfolio-site/src/components/TheFooter.vue
+++ my-portfolio/portfolio-site/src/components/TheFooter.vue
@@ -1,6 +1,10 @@
 <template>
+  <br />
   <footer class="bg-primary shadown-17">
     <p>&copy; 2024 My Portfolio. All Rights Reserved.</p>
+    <a href="https://www.wanderson37.dev" target="_blank">
+      <img src="logow37.svg" />
+    </a>
     <div>
       <a href="https://github.com/Wanderson37" target="_blank">GitHub</a> |
       <a
@@ -11,19 +15,26 @@
     </div>
   </footer>
 </template>
+<script setup></script>
 
 <style>
 footer {
+  display: flex;
   text-align: center;
   padding: 20px;
   color: white;
   border-radius: 1rem;
 }
 footer a {
-  color: #42b983;
+  color: #b9bee6;
   text-decoration: none;
 }
 footer a:hover {
   text-decoration: underline;
+}
+img {
+  width: 50px;
+  height: 50px;
+  filter: drop-shadow(2px 4px 6px rgba(0, 0, 0, 0.3));
 }
 </style>
