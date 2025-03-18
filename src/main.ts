23e1e9d61e7774675d263c7566bbc9f34473b213
Wanderson37 <wanderson_c.o@hotmail.com>
2025-03-18 07:36:11 -0400

    add TypeScript support for Vue components and remove unused style.css

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
-import { createApp } from "vue";
-import { Quasar } from "quasar";
-import "./style.css";
-import App from "./App.vue";
-import "@quasar/extras/material-icons/material-icons.css";
-import "quasar/src/css/index.sass";
+import { createApp } from 'vue'
+import { Quasar } from 'quasar'
+import App from './App.vue'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
 
-const myApp = createApp(App);
+const myApp = createApp(App)
 myApp.use(Quasar, {
-  plugins: {},
-});
+  plugins: {}
+})
 
-myApp.mount("#app");
+myApp.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,16 @@
-import { fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import { quasar, transformAssetUrls } from "@quasar/vite-plugin";
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
 
 export default defineConfig({
   plugins: [
     vue({
-      template: { transformAssetUrls },
+      template: { transformAssetUrls }
     }),
+
     quasar({
-      sassVariables: fileURLToPath(
-        new URL("./src/quasar-variables.sass", import.meta.url)
-      ),
-    }),
-  ],
-});
+      sassVariables: fileURLToPath(new URL('./src/quasar-variables.sass', import.meta.url))
+    })
+  ]
+})
